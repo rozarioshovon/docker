553ccc94507201d7c571b33d29253d3278ea5cc6
Kwon Kiheng <kiheng.kwon@monstar-lab.com>
2016-10-23 05:16:57 +0900

    hbase configuration

--- hadoop/hdfs/master/cmd.sh
+++ hadoop/hdfs/master/cmd.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-sudo -u hdfs /opt/hadoop/sbin/hadoop-daemon.sh start namenode
-sudo -u hdfs yarn resourcemanager
+# hdfs setting
+sudo -u hdfs hdfs namenode -format
 
+sudo -u hdfs /opt/hadoop/sbin/hadoop-daemon.sh start namenode
+sudo -u hdfs /opt/hadoop/sbin/yarn-daemon.sh start resourcemanager
+
+# configure hbase 
+sudo -u hdfs hadoop fs -mkdir /hbase
+sudo -u hdfs hadoop fs -chown -R hdfs:supergroup /hbase
+
+sudo -u hbase /opt/hbase/bin/hbase-daemon.sh start master
+
+while true; do echo my daemonized container; sleep 1; done
--- hadoop/hdfs/slave/cmd.sh
+++ hadoop/hdfs/slave/cmd.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+sudo -u hdfs /opt/hadoop/sbin/hadoop-daemon.sh start datanode
 sudo -u hdfs /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
-sudo -u hdfs hadoop datanode
+sudo -u hbase /opt/hbase/bin/hbase-daemon.sh start regionserver
+
+while true; do echo my daemonized container; sleep 1; done
