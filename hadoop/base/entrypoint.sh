3ccae787fa1710f8c4520572855b54e2eae8822c
Kwon Kiheng <kiheng.kwon@monstar-lab.com>
2016-10-23 03:49:40 +0900

    refactoring docker image

--- hadoop/base/entrypoint.sh
+++ hadoop/base/entrypoint.sh
@@ -4,5 +4,6 @@
 
 sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hadoop/etc/hadoop/core-site.xml
 sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hadoop/etc/hadoop/yarn-site.xml
+sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hbase/conf/hbase-site.xml
 
 exec "$@"
--- hadoop/hbase/base/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hbase/conf/hbase-site.xml
-
-sudo -u hbase hbase "$@" start
-
-
