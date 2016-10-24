6824bdd93ee411657b7e8c3a28790c76577b4474
Kwon Kiheng <kiheng.kwon@monstar-lab.com>
2016-10-24 23:33:00 +0900

    install apache dirll

--- hadoop/hdfs/slave/cmd.sh
+++ hadoop/hdfs/slave/cmd.sh
@@ -3,5 +3,6 @@
 sudo -u hdfs /opt/hadoop/sbin/hadoop-daemon.sh start datanode
 sudo -u hdfs /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
 sudo -u hbase /opt/hbase/bin/hbase-daemon.sh start regionserver
+/opt/drill/bin/drillbit.sh start
 
 while true; do echo my daemonized container; sleep 1; done
