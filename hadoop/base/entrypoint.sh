77beec6a68f9675286192e30e593ff0e764d4a4c
Kiheng Kwon <kiheng.kwon@monstar-lab.com>
2016-10-27 11:45:35 +0900

    install debian version

--- hadoop/base/entrypoint.sh
+++ hadoop/base/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/sbin/sshd
+/etc/init.d/ssh start
 
 sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hadoop/etc/hadoop/core-site.xml
 sed -i 's/%HADOOP_NAMENODE_URI%/namenode/g' /opt/hadoop/etc/hadoop/yarn-site.xml
